56fc89fa1f1b1fdd28b99131ed4a7e156c17a0e4
Davide Corbetta <47659574+Corbe-debug@users.noreply.github.com>
2020-11-26 12:53:02 +0100

    V1 MyFile

--- MyFile/src/myfile/MyFile.java
+++ MyFile/src/myfile/MyFile.java
@@ -9,57 +9,78 @@ import java.io.*;
 import java.util.*;
 
 /**
+ * File management class
  *
  * @author Davide
+ * @version 1.0
  */
 public class MyFile {
 
     /**
-     * @param args the command line arguments
+     * Class constructor
+     *
+     * @author Davide
      */
-    public static void main(String[] args) {
+    public MyFile() {
     }
 
+    /**
+     * Function that permits you to return the file's content in a String
+     *
+     * @author Davide
+     * @param pathFile Path of the file
+     * @return String File's content
+     */
     public String stringReadAllFile(String pathFile) {
         try {
             File name = new File(pathFile);
             BufferedReader input = new BufferedReader(new FileReader(name));
+            ArrayList<String> tempTXT = new ArrayList<>();
             String txt;
             String temp = "";
 
             if (name.isFile()) {
                 while ((txt = input.readLine()) != null) {
-                    if (input.readLine() == null) {
-                        temp += txt;
-                    } else {
-                        temp += txt + "\n";
-                    }
+                    tempTXT.add(txt);
                 }
-
                 input.close();
+            }
+
+            for (int i = 0; i < tempTXT.size(); i++) {
+                if (i == tempTXT.size() - 1) {
+                    temp += tempTXT.get(i);
+                } else {
+                    temp += tempTXT.get(i) + "\n";
+                }
             }
 
             return temp;
 
-        } catch (IOException ioException) {
+        } catch (IOException e) {
             //visualizzo errore
             System.err.println("Read file error");
             return "";
         }
     }
 
+    /**
+     * Function that permits you to return the file's content in a String Arraylist
+     *
+     * @author Davide
+     * @param pathFile Path of the file
+     * @return String Arraylist File's content
+     */
     public ArrayList<String> arrayListReadAllFile(String pathFile) {
         try {
             File name = new File(pathFile);
             BufferedReader input = new BufferedReader(new FileReader(name));
-            ArrayList<String> temp = new ArrayList<String>();
+            ArrayList<String> temp = new ArrayList<>();
             String txt;
 
             if (name.isFile()) {
                 while ((txt = input.readLine()) != null) {
                     temp.add(txt);
                 }
-
                 input.close();
             }
 
@@ -72,28 +93,40 @@ public class MyFile {
         }
     }
 
-    public String stringReadAllFileFromLine(String pathFile, int nRow) {
+    /**
+     * Function that permits you to return file's content from a specific line in a String
+     *
+     * @author Davide
+     * @param pathFile Path of the file
+     * @param nLine Start reading file from this line
+     * @return String File's content from a specific line
+     */
+    public String stringReadAllFileFromLine(String pathFile, int nLine) {
         try {
             File name = new File(pathFile);
             BufferedReader input = new BufferedReader(new FileReader(name));
+            ArrayList<String> tempTXT = new ArrayList<>();
             String txt;
             String temp = "";
 
             if (name.isFile()) {
 
-                for (int i = 0; i < nRow - 1; i++) {
+                for (int i = 0; i < nLine - 1; i++) {
                     input.readLine();
                 }
 
                 while ((txt = input.readLine()) != null) {
-                    if (input == null) {
-                        temp += txt;
-                    } else {
-                        temp += txt + "\n";
-                    }
+                    tempTXT.add(txt);
                 }
-
                 input.close();
+            }
+
+            for (int i = 0; i < tempTXT.size(); i++) {
+                if (i == tempTXT.size() - 1) {
+                    temp += tempTXT.get(i);
+                } else {
+                    temp += tempTXT.get(i) + "\n";
+                }
             }
 
             return temp;
@@ -105,16 +138,24 @@ public class MyFile {
         }
     }
 
-    public ArrayList<String> arrayListReadAllFileFromLine(String pathFile, int nRow) {
+    /**
+     * Function that permits you to return the file's content from a specific line in a String ArrayList
+     *
+     * @author Davide
+     * @param pathFile Path of the file
+     * @param nLine Start reading file from this line
+     * @return String Arraylist File's content from a specific line
+     */
+    public ArrayList<String> arrayListReadAllFileFromLine(String pathFile, int nLine) {
         try {
             File name = new File(pathFile);
             BufferedReader input = new BufferedReader(new FileReader(name));
-            ArrayList<String> temp = new ArrayList<String>();
+            ArrayList<String> temp = new ArrayList<>();
             String txt;
 
             if (name.isFile()) {
 
-                for (int i = 0; i < nRow - 1; i++) {
+                for (int i = 0; i < nLine - 1; i++) {
                     input.readLine();
                 }
 
@@ -133,5 +174,4 @@ public class MyFile {
             return null;
         }
     }
-
 }
